193d4f9c42e6c7e6b7f7741b4b8ae440aa7a8635
Joseph Loper <jd.loper@icloud.com>
2023-10-08 18:59:16 -0700

    Adjust position of Font and Save buttons

--- texteditor.py
+++ texteditor.py
@@ -5,34 +5,35 @@ from tkinter import filedialog, font as tkfont
 root = tk.Tk()
 root.title("ApplePy Text Editor")
 
-text = Text(root)
-text.grid()
-
 
 def saveas():
     global text
     t = text.get("1.0", "end-1c")
     savelocation = filedialog.asksaveasfilename()
-    file1 = open(savelocation, "w+")
-    file1.write(t)
-    file1.close()
+    with open(savelocation, "w+") as file1:
+        file1.write(t)
 
 
 button = Button(root, text="Save", command=saveas)
-button.grid()
+button.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
-# Font chooser
 fonts = list(tkfont.families())
 font_var = tk.StringVar(root)
-font_var.set("SF Mono")  # default value
+font_var.set("SF Mono")
 font_dropdown = OptionMenu(root, font_var, *fonts)
-font_dropdown.grid()
+font_dropdown.grid(row=0, column=1, padx=5, pady=5)
 
 
 def update_font(*args):
-    text.config(font=(font_var.get(), 12))  # assuming font size 12
+    text.config(font=(font_var.get(), 13))
 
 
 font_var.trace("w", update_font)
+
+text = Text(root)
+text.grid(row=1, column=0, columnspan=2, sticky="nsew")
+
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=1)
 
 root.mainloop()
